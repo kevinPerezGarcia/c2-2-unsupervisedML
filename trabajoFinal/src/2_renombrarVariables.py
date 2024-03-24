f745e59fa87aa51be5356b7d7e4ece1f81a3b561
Kevin Perez Garcia <econ.perez.garcia.k@gmail.com>
2024-03-24 10:14:17 -0500

    Añadir archivos fuente
    del 2do al 6to archivo

--- trabajoFinal/src/2_renombrarVariables.py
+++ trabajoFinal/src/2_renombrarVariables.py
@@ -2,7 +2,11 @@ import pandas as pd
 
 df_variablesRelevantes = pd.read_pickle('trabajoFinal/data/interim/df_manteniendo_variablesRelevantes.pkl')
 
-df_renombramiento = pd.read_excel("trabajoFinal/references/diccionarioVariables.xlsx", sheet_name='diccionarioVariables', index_col='Variable original', nrows=41)
+df_renombramiento = pd.read_excel("trabajoFinal/references/diccionarioVariables.xlsx",
+                                  sheet_name='diccionarioVariables',
+                                  usecols=['Variable original', 'Variable renombrada'],
+                                  index_col='Variable original',
+                                  nrows=40)
 
 dict_renombramiento = df_renombramiento['Variable renombrada'].to_dict()
 
